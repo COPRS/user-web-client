65d9f7bf71dbfac071efbdad7828ad11a14706c4
w-cwe <91952300+w-cwe@users.noreply.github.com>
2021-09-17 13:47:27 +0200

    test(filter-sidebar): Add NoopAnimationsModule to fix test

--- src/app/filter-sidebar/filter-sidebar.component.spec.ts
+++ src/app/filter-sidebar/filter-sidebar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FilterSidebarComponent } from './filter-sidebar.component';
 
@@ -8,9 +9,9 @@ describe('FilterSidebarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FilterSidebarComponent ]
-    })
-    .compileComponents();
+      imports: [NoopAnimationsModule],
+      declarations: [FilterSidebarComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
