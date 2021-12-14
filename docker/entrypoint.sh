e651a31ea38797268186e80757531d22ba5fbfbc
w-cwe <91952300+w-cwe@users.noreply.github.com>
2021-12-14 11:59:12 +0100

    RS-116: Make map background configurable in Kubernetes example

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,9 +2,19 @@
 if [ -z "$API_URL" ]
 then
     echo "API_URL is unset";
+    return 1;
 else
     echo "API_URL is set to '$API_URL'";
-    envsubst '$${API_URL}' < /usr/share/nginx/html/assets/config.templ.json > /usr/share/nginx/html/assets/config.json
 fi
+
+if [ -z "$MAP_BACKGROUNDS" ]
+then
+    echo "MAP_BACKGROUNDS is unset";
+    return 1;
+else
+    echo "MAP_BACKGROUNDS is set to '$MAP_BACKGROUNDS'";
+fi
+
+envsubst '$${MAP_BACKGROUNDS} $${MAP_BACKGROUNDS}' < /usr/share/nginx/html/assets/config.templ.json > /usr/share/nginx/html/assets/config.json
 
 exec nginx -g 'daemon off;'
