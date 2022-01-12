874732d6da165b445303ff869eaad074a1c56bde
w-cwe <91952300+w-cwe@users.noreply.github.com>
2022-01-12 10:00:15 +0100

    RS-132: Display in list of footprint is set for products

--- src/app/filter-sidebar/query-result-grid/query-result-grid.component.ts
+++ src/app/filter-sidebar/query-result-grid/query-result-grid.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ClrDatagridStateInterface } from '@clr/angular';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { sequenceEqual, takeUntil } from 'rxjs/operators';
+import { DetailsSidebarNavigationService } from 'src/app/details-sidebar/services/details-sidebar-navigation.service';
 import { DdipProduct } from 'src/app/services/models/DdipProductResponse';
 import { QueryResultService } from '../services/query-result.service';
 
@@ -14,11 +15,15 @@ export class QueryResultGridComponent implements OnInit, OnDestroy {
   products: DdipProduct[];
   total: number;
   pageSize: number = 20;
-  loading: boolean = false;
-  selected: DdipProduct[];
+  loading: boolean = true;
+  public rowSelection: boolean = true;
+  public selected: DdipProduct[] = [];
   private readonly onDestroy = new Subject<void>();
 
-  constructor(private dataService: QueryResultService) {}
+  constructor(
+    private dataService: QueryResultService,
+    private detailsNavService: DetailsSidebarNavigationService
+  ) {}
 
   ngOnInit(): void {
     this.dataService.setPagination(this.pageSize);
@@ -26,8 +31,23 @@ export class QueryResultGridComponent implements OnInit, OnDestroy {
       .getFilteredProducts()
       .pipe(takeUntil(this.onDestroy))
       .subscribe((page) => {
-        this.products = page.products;
+        this.products = page.products as DdipProduct[];
         this.total = page.totalCount;
+        this.loading = false;
+      });
+
+    this.detailsNavService
+      .getSelectedProduct()
+      .pipe(takeUntil(this.onDestroy))
+      .subscribe(async (selected) => {
+        // TODO: select or deselect when changed from outside}
+        // if (!selected) {
+        //   this.selected = [];
+        // } else {
+        //   console.log({ selected });
+        //   // this.selected = this.products.filter((e) => e.Id === selected);
+        //   console.log('selected object', this.selected);
+        // }
       });
   }
 
@@ -39,4 +59,17 @@ export class QueryResultGridComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.onDestroy.next();
   }
+
+  selectionChanged($event) {
+    console.log({ $event });
+    const selectedProduct = $event as DdipProduct;
+    this.detailsNavService.setSelectedProduct(selectedProduct);
+  }
+  trackById(_index: number, item: DdipProduct) {
+    return item.Id;
+  }
 }
+
+// export interface DdipProduct extends DdipProduct {
+//   selected: boolean;
+// }
