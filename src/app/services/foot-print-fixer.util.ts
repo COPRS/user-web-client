74e2b310fc3691d2fed7d2f20ab254563d8cf648
w-cwe <91952300+w-cwe@users.noreply.github.com>
2022-08-10 11:26:15 +0200

    RS-340: Add jsdoc

--- src/app/services/foot-print-fixer.util.ts
+++ src/app/services/foot-print-fixer.util.ts
@@ -1,3 +1,10 @@
+/**
+ * Function to fix footprints that are based on a -180/180 plane
+ * Shifts x values to the right so they can be displayed by OpenLayers
+ *
+ * @param coordinates
+ * @returns
+ */
 export function fixFootprints(
   coordinates: [number, number][]
 ): [number, number][] {
